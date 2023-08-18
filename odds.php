f8e79b66eac8ab3c1a8eee7c99537ece011bdf4c
NotoriousMarc <mobitechconsulting@gmail.com>
2023-08-17 21:29:03 -0500

    added even numbers and divisible by 3

--- odds.php
+++ odds.php
@@ -2,21 +2,23 @@
 
 $totalNumbers = 50;
 $numbers = [];
-
 for ($i=1; $i <= $totalNumbers; $i++) {
-    $numbers[] = $i++;
+    $numbers[] = $i;
 }
 
 printNumbers($numbers);
 
 function printNumbers (array $numbers) : void
 {
-foreach ($numbers as $numbers) {
+    foreach ($numbers as $number) {
+        if (is_float($number/2)) {
+            echo "this number is an odd number " . $number . "." . "<br>";
+        } else {
+            echo "this number is an even number " . $number . "." . "<br>";
+        }
 
-   
-    echo "this number is an odd number " . $numbers . "." . "<br>";
-
-}
-
-
+        if (is_float($number/3) === false) {
+            echo $number . " is divisible by 3." . "<br>";
+        }
+    }
 }
